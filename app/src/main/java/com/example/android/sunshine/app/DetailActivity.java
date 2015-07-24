4ca3194f2423cf7560427c9f1b36500f914e2285
Min Thu Aung <umberto.01@gmail.com>
2015-07-24 16:56:00 -0400

    share button in action bar.

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -19,9 +19,12 @@ package com.example.android.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -70,6 +73,9 @@ public class DetailActivity extends ActionBarActivity {
      */
     public static class DetailFragment extends Fragment {
 
+        private ShareActionProvider mShareActionProvider;
+        private Intent mIntent;
+
         public DetailFragment() {
         }
 
@@ -79,14 +85,38 @@ public class DetailActivity extends ActionBarActivity {
 
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
-            Intent intent = getActivity().getIntent();
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mIntent = getActivity().getIntent();
+            if (mIntent != null && mIntent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecast = mIntent.getStringExtra(Intent.EXTRA_TEXT);
                 TextView detailForecast = (TextView) rootView.findViewById(R.id.detail_forecast);
                 detailForecast.setText(forecast);
             }
 
+            setHasOptionsMenu(true);
+
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            MenuItem item = menu.findItem(R.id.action_share);
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            setShareIntent();
+        }
+
+        public void setShareIntent() {
+            if (mShareActionProvider != null) {
+                if (mIntent != null && mIntent.hasExtra(Intent.EXTRA_TEXT)) {
+                    String forecast = mIntent.getStringExtra(Intent.EXTRA_TEXT);
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, forecast + " #sunshineapp");
+                    mShareActionProvider.setShareIntent(shareIntent);
+                }
+            }
         }
     }
 }
