25a579285144286a7ae1f5a2a3eb10659cc2e368
Min Thu Aung <umberto.01@gmail.com>
2015-08-04 11:28:53 -0400

    implement Content Provider (lesson 4.B)

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -2,12 +2,9 @@ package com.example.android.sunshine.app;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,14 +15,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import org.json.JSONException;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -96,123 +85,12 @@ public class ForecastFragment extends Fragment {
     }
 
     private void fetchWeather() {
-        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask(getActivity(), mForecastAdapter);
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String location = settings.getString(getString(R.string.pref_location_key),
                 getString(R.string.pref_location_default));
         String outputUnits = settings.getString(getString(R.string.pref_units_key),
                 getString(R.string.pref_units_default));
         fetchWeatherTask.execute(location, outputUnits);
-    }
-
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-        @Override
-        protected String[] doInBackground(String... params) {
-
-            // verify params length
-            if (params.length == 0) {
-                return null;
-            }
-
-            /**
-             * Get weather data using openweathermap api
-             */
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            String zip = params[0];
-            String format = "json";
-            String units = "metric"; // always query in metrics (convert to F internally)
-            int dayCount = 7;
-
-            try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily";
-                final String QUERY_PARAM = "q";
-                final String FORMAT_PARAM = "mode";
-                final String UNITS_PARAM = "units";
-                final String DAYS_PARAM = "cnt";
-
-                Uri.Builder builder = new Uri.Builder();
-                builder.encodedPath(FORECAST_BASE_URL)
-                        .appendQueryParameter(FORMAT_PARAM, format)
-                        .appendQueryParameter(QUERY_PARAM, zip)
-                        .appendQueryParameter(UNITS_PARAM, units)
-                        .appendQueryParameter(DAYS_PARAM, Integer.toString(dayCount));
-
-                URL url = new URL(builder.toString());
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-            String[] weatherForecasts = null;
-            try {
-                String outputUnits = params[1];
-                weatherForecasts = WeatherDataParser.getWeatherDataFromJson(forecastJsonStr, outputUnits, dayCount);
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, "Error parsing json", e);
-            }
-            return weatherForecasts;
-        }
-
-        @Override
-        protected void onPostExecute(String[] strings) {
-            if (strings != null) {
-                mForecastAdapter.clear();
-                for (String forecast : strings) {
-                    mForecastAdapter.add(forecast);
-                }
-            }
-        }
     }
 }
